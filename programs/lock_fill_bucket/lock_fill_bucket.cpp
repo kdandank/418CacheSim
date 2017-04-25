2fdc1a0a0fa86297dd66fcbff6df156a765e29f4
Yash Tibrewal <yashykt@gmail.com>
2017-04-25 17:49:25 -0400

    Fill bucket programs

---
+++ programs/lock_fill_bucket/lock_fill_bucket.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <pthread.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int num_threads;
+int num_buckets;
+int num_numbers;
+
+int *buckets;
+int *numbers;
+
+pthread_mutex_t *locks;
+
+void *fill_buckets(void *arg) {
+    pthread_t tid = (pthread_t) arg;
+    int range_start = num_numbers / num_threads * tid;
+    int range_end = (tid == num_threads - 1) ? num_numbers :
+                        range_start + num_numbers / num_threads;
+
+    for(int i = range_start; i < range_end; i++) {
+        pthread_mutex_lock(&locks[numbers[i]]);
+        buckets[numbers[i]]++;
+        pthread_mutex_unlock(&locks[numbers[i]]);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 4) {
+        cout << "Usage : " << argv[0] << " [NUM_THREADS] [NUM_BUCKETS] [NUM_NUMBERS]\n";
+        return -1;
+    }
+
+    num_threads = atoi(argv[1]);
+    num_buckets = atoi(argv[2]);
+    num_numbers = atoi(argv[3]);
+
+    buckets = new int[num_buckets];
+    numbers = new int[num_numbers];
+
+    pthread_t *tids = new pthread_t[num_threads];
+    locks = new pthread_mutex_t[num_buckets];
+
+    //cout << num_threads << " " << num_buckets << " " << num_numbers << "\n";
+
+    /* Initialize locks */
+    for(int i = 0; i < num_buckets; i++) {
+        pthread_mutex_init(&locks[i], NULL);
+    }
+
+    /* Fill numbers with random numbers in num_buckets range */
+    srand(time(0));
+    for(int i = 0; i < num_numbers; i++) {
+        numbers[i] = rand() % num_buckets;
+    }
+
+    for(int i = 0; i < num_threads; i++) {
+        pthread_create(&tids[i], NULL, fill_buckets, (void *) ((long) i ));
+    }
+
+    for(int i = 0; i < num_threads; i++) {
+        pthread_join(tids[i], NULL);
+    }
+
+    /*long sum = 0;
+    for(int i = 0; i < num_buckets; i++) {
+        sum += buckets[i];
+        cout << buckets[i] << "\n";
+    }
+    cout << sum <<"\n";*/
+    return 0;
+}
--- run_pintool.sh
+++ run_pintool.sh
@@ -5,8 +5,10 @@ cd pin-3.2-81205-gcc-linux/source/tools/MyMemTracer/
 
 #run pintool
 #../../../pin -t obj-intel64/MyMemTracer.so -- ~/private/Documents/15618/collab/assignment3/bfs/bfs /afs/cs/academic/class/15418-s17/public/asst3_graphs/tiny.graph
-../../../pin -t obj-intel64/MyMemTracer.so -o lock_add.trace -- ~/private/Documents/15618/collab/final/418CacheSim/programs/lock_add/lock_add 10 30
-../../../pin -t obj-intel64/MyMemTracer.so -o wild_add.trace -- ~/private/Documents/15618/collab/final/418CacheSim/programs/wild_add/wild_add 10 30
+../../../pin -t obj-intel64/MyMemTracer.so -o lock_add.trace -- ../../../../programs/lock_add/lock_add 10 30
+../../../pin -t obj-intel64/MyMemTracer.so -o wild_add.trace -- ../../../../programs/wild_add/wild_add 10 30
+../../../pin -t obj-intel64/MyMemTracer.so -o wild_fill_bucket.trace -- ../../../../programs/wild_fill_bucket/wild_fill_bucket 10 100 1000
+../../../pin -t obj-intel64/MyMemTracer.so -o lock_fill_bucket.trace -- ../../../../programs/lock_fill_bucket/lock_fill_bucket 10 100 1000
 
 # copy traces
 cp -r *.trace ../../../../traces/
