d3bb8c9ff8c1e5ba6224cfd3651b66c87ca41598
kshitizdange <kshitiz.dange@gmail.com>
2017-04-25 20:40:35 -0400

    modified run_pintool to support new changes and make

--- run_pintool.sh
+++ run_pintool.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
+# make tests
+cd programs/bfs/
+#make
+cd ../lock_add
+make
+cd ../wild_add
+make
+cd ../lock_fill_bucket
+make
+cd ../wild_fill_bucket
+make
+cd ../../
+
 # enter pintool dir
 cd pin-3.2-81205-gcc-linux/source/tools/MyMemTracer/
+make
 
 #run pintool
-#../../../pin -t obj-intel64/MyMemTracer.so -- ~/private/Documents/15618/collab/assignment3/bfs/bfs /afs/cs/academic/class/15418-s17/public/asst3_graphs/tiny.graph
-../../../pin -t obj-intel64/MyMemTracer.so -o lock_add.trace -- ../../../../programs/lock_add/lock_add 10 30
-../../../pin -t obj-intel64/MyMemTracer.so -o wild_add.trace -- ../../../../programs/wild_add/wild_add 10 30
-../../../pin -t obj-intel64/MyMemTracer.so -o wild_fill_bucket.trace -- ../../../../programs/wild_fill_bucket/wild_fill_bucket 10 100 1000
-../../../pin -t obj-intel64/MyMemTracer.so -o lock_fill_bucket.trace -- ../../../../programs/lock_fill_bucket/lock_fill_bucket 10 100 1000
+#../../../pin -t obj-intel64/TraceFuncsList.so -- ~/private/Documents/15618/collab/assignment3/bfs/bfs /afs/cs/academic/class/15418-s17/public/asst3_graphs/tiny.graph
+../../../pin -t obj-intel64/TraceFuncsList.so -o lock_add.trace -- ../../../../programs/lock_add/lock_add 10 30
+../../../pin -t obj-intel64/TraceFuncsList.so -o wild_add.trace -- ../../../../programs/wild_add/wild_add 10 30
+../../../pin -t obj-intel64/TraceFuncsList.so -o wild_fill_bucket.trace -- ../../../../programs/wild_fill_bucket/wild_fill_bucket 10 100 1000
+../../../pin -t obj-intel64/TraceFuncsList.so -o lock_fill_bucket.trace -- ../../../../programs/lock_fill_bucket/lock_fill_bucket 10 100 1000
 
 # copy traces
 cp -r *.trace ../../../../traces/
 
 # return to main dir
 cd ../../../../
+
+# clean tests
+cd programs/bfs/
+#make clean
+cd ../lock_add
+make clean
+cd ../wild_add
+make clean
+cd ../lock_fill_bucket
+make clean
+cd ../wild_fill_bucket
+make clean
+cd ../../
 
