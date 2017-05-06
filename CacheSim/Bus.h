1938e4e86bc981860a8009a40f7c7d837f914fb2
kshitizdange <kshitiz.dange@gmail.com>
2017-05-06 17:23:23 -0400

    pervious commit had unsynced changes, bus code compiles now

--- CacheSim/Bus.h
+++ CacheSim/Bus.h
@@ -3,21 +3,25 @@
 
 #include <pthread.h>
 
-class Bus {
-
-public:
-    static pthread_mutex_t lock;
-    static pthread_cond_t  cond_var;
-    static int resp_count;
-    static int owner_id;
-
-    static enum operations {
+enum operations {
         BusRd = 0,
         BusRdx,
         BusWr,
         BusWrx,
         BusUpd
-    }opts;
+};
+
+class Bus {
+
+public:
+    static pthread_mutex_t lock;
+    static pthread_cond_t  cvar;
+    static int resp_count;
+    static int owner_id;
+
+    static operations opt;
 
     static void init();
 };
+
+#endif /* _BUS_H_ */
