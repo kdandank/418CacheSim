b3e226ed0d5706706ddbcb7c977b0d2408967f69
kshitizdange <kshitiz.dange@gmail.com>
2017-05-09 20:23:33 -0400

    Traces from GHC and run_pin_tool modified

--- run_pintool.sh
+++ run_pintool.sh
@@ -21,11 +21,11 @@ make
 
 #run pintool
 #../../../pin -t obj-intel64/TraceFuncsList.so -- ~/private/Documents/15618/collab/assignment3/bfs/bfs /afs/cs/academic/class/15418-s17/public/asst3_graphs/tiny.graph
-../../../pin -t obj-intel64/TraceFuncsList.so -o lock_add.trace -- ../../../../programs/lock_add/lock_add 10 30
-../../../pin -t obj-intel64/TraceFuncsList.so -o wild_add.trace -- ../../../../programs/wild_add/wild_add 10 30
-../../../pin -t obj-intel64/TraceFuncsList.so -o wild_fill_bucket.trace -- ../../../../programs/wild_fill_bucket/wild_fill_bucket 10 100 1000
-../../../pin -t obj-intel64/TraceFuncsList.so -o lock_fill_bucket.trace -- ../../../../programs/lock_fill_bucket/lock_fill_bucket 10 100 1000
-../../../pin -t obj-intel64/TraceFuncsList.so -o reader_writer.trace -- ../../../../programs/reader_writer/reader_writer 10 10000
+../../../pin -t obj-intel64/TraceFuncsList.so -o lock_add.trace -- ../../../../programs/lock_add/lock_add 16 30
+../../../pin -t obj-intel64/TraceFuncsList.so -o wild_add.trace -- ../../../../programs/wild_add/wild_add 16 30
+../../../pin -t obj-intel64/TraceFuncsList.so -o wild_fill_bucket.trace -- ../../../../programs/wild_fill_bucket/wild_fill_bucket 16 100 1000
+../../../pin -t obj-intel64/TraceFuncsList.so -o lock_fill_bucket.trace -- ../../../../programs/lock_fill_bucket/lock_fill_bucket 16 100 1000
+../../../pin -t obj-intel64/TraceFuncsList.so -o reader_writer.trace -- ../../../../programs/reader_writer/reader_writer 16 10000
 
 # copy traces
 cp -r *.trace ../../../../traces/
